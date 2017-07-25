a214c0fba6bae84f70284ff4414d9e0cd247c81c
Ahmad Iqbal Ali <ahmadiq@users.noreply.github.com>
2017-07-25 15:07:19 +0500

    fix sed expression

--- 4.5/start.sh
+++ 4.5/start.sh
@@ -3,11 +3,11 @@
 CMD="kibana"
 set -e
 if [[ -n "$ELASTICSEARCH_URL" ]]; then
-  sed -i 's|^\(#\+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"$ELASTICSEARCH_URL\""'|' /opt/kibana/config/kibana.yml
+  sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"$ELASTICSEARCH_URL\""'|' /opt/kibana/config/kibana.yml
 elif [[ -n "$ELASTICSEARCH_SERVICE_NAME" ]]; then
   SVC_HOST=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_HOST
   SVC_PORT=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_PORT
-  sed -i 's|^\(#\+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana/config/kibana.yml
+  sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana/config/kibana.yml
 fi
 
 set -- gosu kibana tini -- $CMD
