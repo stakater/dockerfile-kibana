d598af09c191df33cbe7f430c23d4f5d279c9aa9
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-16 15:38:19 +0500

    Add server_basepath

--- 4.5/start.sh
+++ 4.5/start.sh
@@ -6,6 +6,10 @@ if [ "$ELASTICSEARCH_URL" ]; then
 	sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
 fi
 
+if [ "$SERVER_BASEPATH" ]; then
+	sed -ri "s!^(\#\s*)?(server\.basePath:).*!\2 '$SERVER_BASEPATH'!" /opt/kibana/config/kibana.yml
+fi
+
 set -- gosu kibana tini -- $CMD
 
 exec $CMD
